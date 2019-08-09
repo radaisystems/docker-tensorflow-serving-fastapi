7ac5b228e3da22293dc651f4e30738f51e0e3c93
Robert Hafner <tedivm@tedivm.com>
2019-08-08 17:14:52 -0700

    Allow timeout to be overridden

--- docker/tfs/start_tfs.sh
+++ docker/tfs/start_tfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tensorflow_model_server --port=8500 --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=${MODEL_BASE_PATH}/${MODEL_NAME} "$@"
+tensorflow_model_server --rest_api_timeout_in_ms=${REST_API_TIMOUT} --port=8500 --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=${MODEL_BASE_PATH}/${MODEL_NAME} "$@"
