3b80c29df4f4531849eaacd680cfebe238219f06
Robert Hafner <tedivm@tedivm.com>
2019-11-13 12:57:55 -0800

    Add tensorflow 1.15.0 containers, stop building 1.13.0

--- docker/tfs/start_tfs.sh
+++ docker/tfs/start_tfs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-BATCH_PARAMETER_FILE='/tmp/batch_params.txt'
+BATCH_PARAMETER_FILE='/tmp/batch_params.pb'
 BASE_COMMAND="tensorflow_model_server --rest_api_timeout_in_ms=${REST_API_TIMEOUT} --port=8500 --rest_api_port=8501 --model_name=${MODEL_NAME} --model_base_path=${MODEL_BASE_PATH}/${MODEL_NAME}"
+
+if [ ! -z "$REST_API_TIMEOUT" ]; then
+  BASE_COMMAND="$BASE_COMMAND --rest_api_timeout_in_ms=${REST_API_TIMEOUT}"
+fi
 
 if [ "$ENABLE_BATCHING" = 'true' ]; then
   HAS_PARAMETERS=false
